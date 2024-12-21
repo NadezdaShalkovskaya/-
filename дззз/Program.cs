5f8ddbbe589fad3a71b5a385835edee2ba3a2f1e
Надежда <@DESKTOP-EHSPKBR>
2024-12-21 19:49:09 +0200

    Добавьте файлы проекта.

---
+++ дззз/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+      string input = "Пример строки";
+        if (input.Length > 0)
+        { 
+            string result = " " +
+                " " + input.Substring(1, input.Length - 2) + ".";
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Строка пуста");
+        }
+    } 
+}
+   
